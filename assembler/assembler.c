4731e6b072f98769df4b780a8cbfe5ba4b5ec22c
terence matsune <matsuneterence@gmail.com>
2024-07-11 11:50:48 -0700

    assembler fixed

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "assembler.h"
 #include "utils.h"
 #include "parser.h"
@@ -29,12 +30,12 @@ static void term_declare_variable(struct term_node *term, dynamic_array *variabl
 static void expression_declare_variable(struct expression_node *expression, dynamic_array *variables){
     switch(expression->type){
         case EXPRESSION_TERM: {
-            term_declare_variable(&expression->term, &variables);
+            term_declare_variable(&expression->term, variables);
             break;
         }
         case EXPRESSION_PLUS: {
-            term_declare_variable(&expression->add.left, &variables);
-            term_declare_variable(&expression->add.right, &variables);
+            term_declare_variable(&expression->add.left, variables);
+            term_declare_variable(&expression->add.right, variables);
             break;
         }
     }
@@ -54,10 +55,10 @@ static void instruction_declare_variable(struct instruction_node *instruction, d
     switch(instruction->type){
         case INSTR_ASSIGN: {   
             // This ensures that all variables used in the expression being assigned are declared
-            expression_declare_variable(&instruction->assign, variables);
+            expression_declare_variable(&instruction->assign.expression, variables);
             for (unsigned int i = 0; i < variables->count; i++){
                 char *variable = NULL;
-                ds_dynamic_array_get(variables, i, &variable);
+                array_get(variables, i);
                 if (strcmp(instruction->assign.identifier, variable) == 0) {
                     return;
                 }
@@ -98,7 +99,7 @@ void program_asm(struct program_node *program){
     struct instruction_node *instruction;
     for(int i = 0; i < program->instructions.count; i++){
         instruction = (struct instruction_node *)array_get(&program->instructions, i);
-        instruction_declare_variable(&instruction, &variables);
+        instruction_declare_variable(instruction, &variables);
     }
 
 }
